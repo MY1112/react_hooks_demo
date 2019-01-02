fce66ee4c188830599717dea41ae5dfe9fa17275
Circlemeng <mengyuan@neurongenius.com>
2019-01-02 18:32:28 +0800

    修改页面样式

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { TransitionGroup } from 'react-transition-group';
 import GSAP from 'react-gsap-enhancer'
 import { TimelineMax, Sine } from 'gsap';
@@ -21,7 +21,7 @@ class Photo extends Component {
             .from(utils.target, 1, {
                 x: `+=${( 4 - id ) * 60}px`,
                 autoAlpha: 0,
-                onComplete: utils.options.callback,
+                onComplete: utils.options.callback
             }, id * 0.7);
     }
 
@@ -32,7 +32,7 @@ class Photo extends Component {
                 scale: 0,
                 ease: Sine.easeOut,
                 onComplete: utils.options.callback,
-            }, (4 - id) * 0.7);
+            }, (4 - id) * 0.5);
     }
 
     render() {
@@ -47,49 +47,36 @@ class Photo extends Component {
 
 const WrappedPhoto = GSAP()(Photo);
 
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            show: false,
-            photos: [{
-                id: 1,
-                url: 'http://img4.imgtn.bdimg.com/it/u=1032683424,3204785822&fm=214&gp=0.jpg'
-            }, {
-                id: 2,
-                url: 'http://scimg.jb51.net/allimg/160714/103-160G40912112a.jpg'
-            }, {
-                id: 3,
-                url: 'http://img.name2012.com/uploads/allimg/180430/233223B52-2.jpg'
-            }, {
-                id: 4,
-                url: 'http://img4.imgtn.bdimg.com/it/u=360498760,1598118672&fm=27&gp=0.jpg'
-            }]
-        };
-    }
+export default function App()  {
+    const [show, setShow] = useState(false)
+    const [photos, setPhotos] = useState(
+        [{
+            id: 1,
+            url: 'http://img4.imgtn.bdimg.com/it/u=1032683424,3204785822&fm=214&gp=0.jpg'
+        }, {
+            id: 2,
+            url: 'http://scimg.jb51.net/allimg/160714/103-160G40912112a.jpg'
+        }, {
+            id: 3,
+            url: 'http://img.name2012.com/uploads/allimg/180430/233223B52-2.jpg'
+        }, {
+            id: 4,
+            url: 'http://img4.imgtn.bdimg.com/it/u=360498760,1598118672&fm=27&gp=0.jpg'
+        }]
+    )
 
-    toggle = () => {
-        this.setState({
-            show: !this.state.show
+    const renderPhotos = () => {
+        return photos.map((item, index) => {
+            return <WrappedPhoto id={item.id} url={item.url} key={`photo${item.id}`} />;
         })
     }
 
-    render() {
-        const { show, photos } = this.state;
-
-        const renderPhotos = () => {
-            return photos.map((item, index) => {
-                return <WrappedPhoto id={item.id} url={item.url} key={`photo${item.id}`} />;
-            })
-        }
-
-        return (
-            <div className="demo-hook">
-                <button onClick={this.toggle}>toggle</button>
-                <TransitionGroup component="div">
-                    {show && renderPhotos()}
-                </TransitionGroup>
-            </div>
-        );
-    }
+    return (
+        <div className="demo-hook">
+            <button onClick={() => setShow(!show)}>Jump Egg</button>
+            <TransitionGroup component="div">
+                {show && renderPhotos()}
+            </TransitionGroup>
+        </div>
+    );
 }
