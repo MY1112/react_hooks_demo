14cc629ff7fd4a481eefd173f21f57049ffbbe5e
Circlemeng <mengyuan@neurongenius.com>
2019-01-03 18:03:57 +0800

    增加窗口宽高的监听

--- src/App.js
+++ src/App.js
@@ -5,34 +5,34 @@ import { TimelineMax, Sine } from 'gsap';
 
 import './App.css';
 
-class Photo extends Component {
-
-    componentWillEnter(callback) {
-        this.addAnimation(this.enterAnim, {callback: callback})
-    }
-
-    componentWillLeave(callback) {
-        this.addAnimation(this.leaveAnim, {callback: callback})
-    }
-
-    enterAnim = (utils) => {
-        const { id } = this.props;
-        return new TimelineMax()
+function enterAnim(props, utils) {
+    const { id } = props
+    return new TimelineMax()
             .from(utils.target, 1, {
                 x: `+=${( 4 - id ) * 60}px`,
                 autoAlpha: 0,
                 onComplete: utils.options.callback
             }, id * 0.7);
-    }
+}
 
-    leaveAnim = (utils) => {
-        const { id } = this.props;
+function leaveAnim(props, utils) {
+    const { id } = props
         return new TimelineMax()
             .to(utils.target, 0.5, {
                 scale: 0,
                 ease: Sine.easeOut,
                 onComplete: utils.options.callback,
             }, (4 - id) * 0.5);
+}
+
+class Photo extends Component {
+
+    componentWillEnter(callback) {
+        this.addAnimation(enterAnim.bind(this, this.props), {callback: callback})
+    }
+
+    componentWillLeave(callback) {
+        this.addAnimation(leaveAnim.bind(this, this.props), {callback: callback})
     }
 
     render() {
@@ -47,10 +47,35 @@ class Photo extends Component {
 
 const WrappedPhoto = GSAP()(Photo);
 
+function getSize() {
+    return {
+        innerHeight: window.innerHeight,
+        innerWidth: window.innerWidth,
+        outerHeight: window.outerHeight,
+        outerWidth: window.outerWidth
+    };
+}
+
+function useWindowSize() {
+    let [windowSize, setWindowSize] = useState(getSize());
+  
+    function handleResize() {
+        setWindowSize(getSize());
+    }
+  
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+  
+    return windowSize;
+}
+  
 export default function App()  {
     const [show, setShow] = useState(false)
-    const [photos, setPhotos] = useState(
-        [{
+    const photos = [{
             id: 1,
             url: 'http://img4.imgtn.bdimg.com/it/u=1032683424,3204785822&fm=214&gp=0.jpg'
         }, {
@@ -62,8 +87,7 @@ export default function App()  {
         }, {
             id: 4,
             url: 'http://img4.imgtn.bdimg.com/it/u=360498760,1598118672&fm=27&gp=0.jpg'
-        }]
-    )
+        }];
 
     const renderPhotos = () => {
         return photos.map((item, index) => {
@@ -71,12 +95,24 @@ export default function App()  {
         })
     }
 
+    const windowSize = useWindowSize()
+
+
     return (
-        <div className="demo-hook">
-            <button onClick={() => setShow(!show)}>Jump Egg</button>
-            <TransitionGroup component="div">
-                {show && renderPhotos()}
-            </TransitionGroup>
+        <div>
+            <div className="demo-hook">
+                <button onClick={() => setShow(!show)}>Jump Egg</button>
+                <TransitionGroup component="div">
+                    {show && renderPhotos()}
+                </TransitionGroup>
+            </div>
+            <div className="demo-hook-2">
+                <p>innerHeight:{windowSize.innerHeight}</p>
+                <p>innerWidth:{windowSize.innerWidth}</p>
+                <p>outerHeight:{windowSize.outerHeight}</p>
+                <p>outerWidth:{windowSize.outerWidth}</p>
+            </div>
         </div>
     );
 }
+
